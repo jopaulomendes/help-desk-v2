15b721f7d097fc25c2246f57108cb9bb55af8661
João Paulo <jp_cbc@hotmail.com>
2024-01-15 22:53:14 -0300

    Documentação com OpenAPI

---
+++ backend/user-service-api/src/main/java/com/jopaulo/userserviceapi/config/OpenaConfAPI.java
@@ -0,0 +1,23 @@
+package com.jopaulo.userserviceapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenaConfAPI {
+
+    public OpenAPI customOpenApi(
+            @Value("${springdoc.openapi.title}") final String title,
+            @Value("${springdoc.openapi.description}") final String description,
+            @Value("${springdoc.openapi.version}") final String version
+    ){
+        return new OpenAPI()
+                .info(new Info()
+                        .title(title)
+                        .description(description)
+                        .version(version)
+        );
+    }
+}
--- backend/user-service-api/src/main/java/com/jopaulo/userserviceapi/controller/UserController.java
+++ backend/user-service-api/src/main/java/com/jopaulo/userserviceapi/controller/UserController.java
@@ -1,15 +1,41 @@
 package com.jopaulo.userserviceapi.controller;
 
 import com.jopaulo.userserviceapi.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import models.exceptions.StandardError;
 import models.response.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Tag(name = "UserController", description = "Controller responsável pelas operações do usuário")
 @RequestMapping("/api/users")
 public interface UserController {
 
+    @Operation(summary = "Find user by is")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário encontrado"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado",
+                    content = @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = StandardError.class)
+                    )),
+            @ApiResponse(responseCode = "500", description = "Erro interno no servidor",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = StandardError.class)
+                    ))
+    })
     @GetMapping("/{id}")
-    ResponseEntity<UserResponse> findById(@PathVariable final String id);
+    ResponseEntity<UserResponse> findById(
+            @Parameter(description = "User id", required = true, example = "65794a5330a73e4d5e73261b")
+            @PathVariable final String id
+    );
 }
