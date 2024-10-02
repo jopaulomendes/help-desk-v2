2ad7f0de89a801fe5861af6acb38334c15c89e86
João Paulo <jp_cbc@hotmail.com>
2024-10-01 23:24:20 -0300

    Criando Primeira Estrutura no Banco de Dados

--- backend/order-service-api/src/main/java/com/jopaulo/orderserviceapi/OrderServiceApiApplication.java
+++ backend/order-service-api/src/main/java/com/jopaulo/orderserviceapi/OrderServiceApiApplication.java
@@ -3,14 +3,11 @@ package com.jopaulo.orderserviceapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class OrderServiceApiApplication {
     public static void main(String[] args) {
-        try {
-            SpringApplication.run(OrderServiceApiApplication.class, args);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
+        SpringApplication.run(OrderServiceApiApplication.class, args);
     }
 }
---
+++ backend/order-service-api/src/main/java/com/jopaulo/orderserviceapi/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.jopaulo.orderserviceapi.repositories;
+
+import com.jopaulo.orderserviceapi.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
