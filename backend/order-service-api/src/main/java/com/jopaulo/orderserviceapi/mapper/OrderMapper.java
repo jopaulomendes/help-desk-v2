62593c08b14678cb152d00843214d02654ca8b00
João Paulo <jp_cbc@hotmail.com>
2024-10-16 23:52:55 -0300

    Bugfix na Criação de Ordens de Serviço

--- backend/order-service-api/src/main/java/com/jopaulo/orderserviceapi/mapper/OrderMapper.java
+++ backend/order-service-api/src/main/java/com/jopaulo/orderserviceapi/mapper/OrderMapper.java
@@ -8,6 +8,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
+import java.time.LocalDateTime;
+
 import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
 import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
 
@@ -21,10 +23,15 @@ public interface OrderMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "status", source = "status", qualifiedByName = "mapStatus")
+    @Mapping(target = "createdAt", expression = "java(mapCreatedAt())")
     Order fromRequest(CreateOrderRequest request);
 
     @Named("mapStatus")
     default OrderStatusEnum mapStatus(final String status){
         return OrderStatusEnum.toEnum(status);
+    }
+
+    default LocalDateTime mapCreatedAt() {
+        return LocalDateTime.now();
     }
 }
