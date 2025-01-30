7a0f2281408b6dfed2250e8371e2e3a6f2e1770e
João Paulo <jp_cbc@hotmail.com>
2025-01-29 23:53:50 -0300

    Start do Serviço de BFF

---
+++ backend/helpdesk-bff/src/main/java/com/jopaulo/helpdeskbff/HelpdeslBffApplication.java
@@ -0,0 +1,13 @@
+package com.jopaulo.helpdeskbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeslBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeslBffApplication.class, args);
+	}
+
+}
---
+++ backend/helpdesk-bff/src/test/java/com/jopaulo/helpdeskbff/HelpdeslBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jopaulo.helpdeskbff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeslBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
