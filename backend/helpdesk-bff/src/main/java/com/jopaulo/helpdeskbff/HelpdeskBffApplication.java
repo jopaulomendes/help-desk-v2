8c1e5433232c4d6ac3af15f1a6a0d2e2a5e5d5cd
João Paulo <jp_cbc@hotmail.com>
2025-01-30 01:04:50 -0300

    Propriedades de Conexão com Config Server e Service Discovery

--- backend/helpdesk-bff/src/main/java/com/jopaulo/helpdeskbff/HelpdeskBffApplication.java
+++ backend/helpdesk-bff/src/main/java/com/jopaulo/helpdeskbff/HelpdeskBffApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelpdeslBffApplication {
+public class HelpdeskBffApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelpdeslBffApplication.class, args);
+		SpringApplication.run(HelpdeskBffApplication.class, args);
 	}
 
 }
